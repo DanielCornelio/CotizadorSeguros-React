8fd2c84085a80e840502eeed811136e63a0eede5
DanielCornelio <jdcornelio89@gmail.com>
2019-10-02 14:41:55 -0500

    Resumen de Cotizació

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
 import Header from './Header';
 import Formulario from './Formulario';
+import Resumen from './Resumen';
 import {obtenerDiferenciaAnio, calcularMarca, obtenerPlan} from '../helper';
 
 class App extends Component {
+  state ={
+    resultado:'',
+    datos: {}
+  }
   cotizarSeguro = (datos) => {
     const {marca, plan, year} = datos;
     //agregar una base de 2000
@@ -18,7 +23,17 @@ class App extends Component {
     let incrementoPlan = obtenerPlan(plan);
     //dependiendo del plan incrementar
     resultado = parseFloat(incrementoPlan * resultado).toFixed(2);
-    console.log(resultado);
+    //crear objetos para el resumen
+    const datosAuto ={
+      marca:marca,
+      plan:plan,
+      year:year
+    }
+    // ya tenemos el costo
+    this.setState({
+      resultado: resultado,
+      datos: datosAuto
+    })
   }
   render(){
     return (
@@ -29,6 +44,10 @@ class App extends Component {
         <div className="contenedor-formulario">
           <Formulario
             cotizarSeguro = {this.cotizarSeguro}
+          />
+          <Resumen
+            datos= {this.state.datos}
+            resultado= {this.state.resultado}
           />
         </div>
       </div>
